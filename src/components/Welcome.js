181676825e781399a346faed1e27c9aefac6708d
snehapatel123-oss <116440431+snehapatel123-oss@users.noreply.github.com>
2023-02-13 14:33:35 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,13 @@
 // write code for Welcome component here
+import React from 'react';
+
+const Welcome = ({name}) => {
+    let sayHi = `Hey ${name}!`;
+    let greetMessage = `Welcome to Newton School.`;
+    return (
+        <div>
+            <h1>{sayHi}</h1>
+            <h2>{greetMessage}</h2>
+        </div>
+    );
+};
